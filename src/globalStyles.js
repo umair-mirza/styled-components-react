bde2381c45efa8a16033d73efe6753070fb2d95b
Umair Mirza <umair_hp@hotmail.com>
2022-07-30 16:36:58 +0500

    added global styles

---
+++ src/globalStyles.js
@@ -0,0 +1,10 @@
+import styled, {createGlobalStyle} from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-family: 'Montserrat', sans-serif;
+    }
+`
