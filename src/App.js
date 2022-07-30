ffdded18f33972930f891d45386aed768a727850
Umair Mirza <umair_hp@hotmail.com>
2022-07-31 00:28:51 +0500

    completed the cards layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { ThemeProvider } from 'styled-components'
 import Header from './components/Header'
+import Card from './components/Card'
 import { Container } from './components/styles/Container.styled'
 import GlobalStyles from './components/styles/globalStyles'
+import content from './content'
 
  const theme = {
   colors: {
@@ -19,7 +21,9 @@ function App() {
       <GlobalStyles />
       <Header />
       <Container>
-
+        {content.map((item, index) => (
+          <Card key={index} item={item} />
+        ))}
       </Container>
       </>
     </ThemeProvider>
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import { Container } from "./styles/Container.styled";
+import { Flex } from "./styles/Flex.styled";
+
+const Footer = () => {
+    return (
+        <div>
+            <Container>
+                <img src="./images/logo_white.svg" alt="" />
+
+                <Flex>
+                    
+                </Flex>
+            </Container>
+        </div>
+    )
+}
+
+export default Footer
